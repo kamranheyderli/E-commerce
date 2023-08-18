296246b430c5198ca074a74c05b2273863c96054
kamran0599 <kamranheyderli049@gmail.com>
2023-08-18 15:47:48 +0400

    add_task

--- src/redux/root/addToCardSlice.js
+++ src/redux/root/addToCardSlice.js
@@ -30,18 +30,15 @@ const cartSlice = createSlice({
         
             if (existingItemIndex !== -1) {
                 const existingItem = state.items[existingItemIndex];
-        
-                if (existingItem.count === 1) {
-                    state.totalQuantity -= 1;
-                    state.items.splice(existingItemIndex, 1);
-                } else {
-                    existingItem.count -= 1;
-                    state.totalQuantity -= 1; // Aynı ID'ye sahip ürün çıkarılınca totalQuantity'yi azalt
-                }
-        
-                state.totalPrice -= existingItem.price;
+                
+                state.totalQuantity -= existingItem.count;
+                state.totalPrice -= existingItem.count * existingItem.price;
+                
+                state.items.splice(existingItemIndex, 1);
+                
                 localStorage.setItem('totalQuantity', state.totalQuantity.toString());
                 localStorage.setItem('totalPrice', state.totalPrice.toString());
+                localStorage.setItem('cart', JSON.stringify(state.items));
             }
         },
         incrementItem: (state, action) => {
@@ -63,7 +60,7 @@ const cartSlice = createSlice({
             if (existingItem) {
                 if (existingItem.count === 1) {
                     state.totalQuantity -= 1;
-                    state.totalPrice -= existingItem.price; // Deduct the item's price from the total price
+                    state.totalPrice -= existingItem.price; 
                     state.items = state.items.filter(item => item.id !== itemIdToDecrement);
                 } else {
                     existingItem.count -= 1;
@@ -72,7 +69,7 @@ const cartSlice = createSlice({
                 }
                 
                 if (existingItem.count === 0) {
-                    existingItem.price = 0; // Set the item's price to 0 when count is 0
+                    existingItem.price = 0; 
                 }
                 
                 localStorage.setItem('totalQuantity', state.totalQuantity.toString());
